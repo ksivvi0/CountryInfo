e4ef9778eeb14d86c958ff3b35f93d56a21e00f7
ksivvi0 <ksivvi0@gmail.com>
2021-07-07 15:11:33 +0500

    Добавил класс логгер

---
+++ CountryInfo/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryInfo
+{
+    public class Logger
+    {
+        static string LogPath;
+
+        static Logger()
+        {
+            LogPath = "country_info.log";
+        }
+
+        public static async void WriteLogAsync(string data)
+        {
+            using (StreamWriter writer = new StreamWriter(LogPath, true, Encoding.UTF8))
+                await writer.WriteLineAsync($"{DateTime.Now}:\t{data}");
+        }
+    }
+
+}
---
+++ CountryInfo/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace CountryInfo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelLayout = new System.Windows.Forms.Panel();
+            this.CountryTextBox = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.ShowAllBtn = new System.Windows.Forms.Button();
+            this.PanelLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelLayout
+            // 
+            this.PanelLayout.Controls.Add(this.ShowAllBtn);
+            this.PanelLayout.Controls.Add(this.SearchBtn);
+            this.PanelLayout.Controls.Add(this.CountryTextBox);
+            this.PanelLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelLayout.Location = new System.Drawing.Point(0, 0);
+            this.PanelLayout.Name = "PanelLayout";
+            this.PanelLayout.Size = new System.Drawing.Size(562, 463);
+            this.PanelLayout.TabIndex = 0;
+            // 
+            // CountryTextBox
+            // 
+            this.CountryTextBox.Location = new System.Drawing.Point(5, 4);
+            this.CountryTextBox.Name = "CountryTextBox";
+            this.CountryTextBox.Size = new System.Drawing.Size(269, 23);
+            this.CountryTextBox.TabIndex = 0;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Location = new System.Drawing.Point(280, 4);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(100, 25);
+            this.SearchBtn.TabIndex = 1;
+            this.SearchBtn.Text = "Поиск";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            // 
+            // ShowAllBtn
+            // 
+            this.ShowAllBtn.Location = new System.Drawing.Point(386, 4);
+            this.ShowAllBtn.Name = "ShowAllBtn";
+            this.ShowAllBtn.Size = new System.Drawing.Size(172, 25);
+            this.ShowAllBtn.TabIndex = 2;
+            this.ShowAllBtn.Text = "Показать все страны";
+            this.ShowAllBtn.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 463);
+            this.Controls.Add(this.PanelLayout);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CountryInfo";
+            this.PanelLayout.ResumeLayout(false);
+            this.PanelLayout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelLayout;
+        private System.Windows.Forms.Button ShowAllBtn;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.TextBox CountryTextBox;
+    }
+}
+
